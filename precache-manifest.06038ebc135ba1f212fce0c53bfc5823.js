07f0783888aaa333bfe2772273be2947772bad76
FloUz-ai <83022872+FloUz-ai@users.noreply.github.com>
2021-07-18 00:36:23 +0200

    Updates

---
+++ precache-manifest.06038ebc135ba1f212fce0c53bfc5823.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f487b77736d8dd1bf5ea768907cf7b1",
+    "url": "/defi3Alyra/index.html"
+  },
+  {
+    "revision": "be119b981b54bb83a8be",
+    "url": "/defi3Alyra/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "b670e9470045e10f3500",
+    "url": "/defi3Alyra/static/js/2.da6c18c7.chunk.js"
+  },
+  {
+    "revision": "be119b981b54bb83a8be",
+    "url": "/defi3Alyra/static/js/main.fe78fa96.chunk.js"
+  },
+  {
+    "revision": "d3e2c3b663a8f808b242",
+    "url": "/defi3Alyra/static/js/runtime-main.63db1897.js"
+  }
+]);
